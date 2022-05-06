63a6cfbc1c45402d2be041859fc1ff1981686a5c
DevAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-05-06 18:19:41 -0400

    instalei EfCore

---
+++ LanchesMac/context/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
